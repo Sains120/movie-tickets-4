cda097a928779b15264a9b36c21cdf2f04f9ead2
Sains120 <sainsburys@middleton.school.nz>
2023-03-14 10:12:38 +1300

    changes

--- Movie tickets version1.py
+++ Movie tickets version1.py
@@ -1,1 +1,21 @@
-print("hey")
+"""Movie theatre ticketing system - v1
+Welcome screen and variables
+Created by Sophie Sainsbury
+"""
+
+
+# Component 1 - Welcome screen and set up variables
+def sell_ticket():
+    print("********** Fanfare Movies - ticketing system **********\n")
+
+
+adult_tickets = 0
+child_tickets = 0
+student_tickets = 0
+gift_tickets = 0
+tickets_sold = 0
+total_sales = 0
+
+
+# Main routine
+sell_ticket()
